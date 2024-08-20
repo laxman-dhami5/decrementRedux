11a7f264713356c7be6cde3db393cef3f38a6926
laxman-dhami <alexdhami@gmail.com>
2024-08-20 11:16:44 +0530

    decRedux

--- redux-demo.js
+++ redux-demo.js
@@ -2,9 +2,12 @@ const redux = require('redux');
 
 // Reducer function to manage the counter state
 const counterReducer = (state = { counter: 0 }, action) => {
-    return {
-        counter: state.counter + 1
-    };
+    if (action.type === 'increment') {
+        return { counter: state.counter + 1 };
+    } else if (action.type === 'decrement') {
+        return { counter: state.counter - 1 };
+    }
+    
 };
 
 // Creating a Redux store with the counterReducer
@@ -20,10 +23,6 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber);
 
 // Dispatching the 'increment' action 5 times
-store.dispatch({ type: 'increment' });
-store.dispatch({ type: 'increment' });
-store.dispatch({ type: 'increment' });
-store.dispatch({ type: 'increment' });
 store.dispatch({ type: 'increment' });
 
 
